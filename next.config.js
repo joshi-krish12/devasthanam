126993874c03e568c897bcb6793f91dbfe0c119b
krishjoshi <joshikrish795@gmail.com>
2025-10-28 22:40:08 +0530

    Fixed the image rendering issue

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true
+    // Enable Next.js Image Optimization
+    unoptimized: false,
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [360, 640, 768, 1024, 1280, 1536, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
+    domains: ['images.unsplash.com']
   },
 }
 
